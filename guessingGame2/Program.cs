8922f57ca9498a2e426a394a5995d5d37686efbf
Fredrik Leemann <fredrik@leemann.se>
2019-02-28 15:38:40 +0100

    Added a start screen

--- guessingGame2/Program.cs
+++ guessingGame2/Program.cs
@@ -8,6 +8,7 @@ namespace guessingGame2
 
         static void Main(string[] args)
         {
+            welcomeScreen();
             while (gamePlaying)
             {
                 myGame();
@@ -40,10 +41,10 @@ namespace guessingGame2
             Random cpRandomIntGenerator = new Random();
             int cpRandomInt = cpRandomIntGenerator.Next(1, 100);
 
-            // Uncomment to print the random number to screen
-            // Console.WriteLine($"Your random number is: {cpRandomInt}");
+        // Uncomment to print the random number to screen
+        // Console.WriteLine($"Your random number is: {cpRandomInt}");
 
-            askAgain:
+        askAgain:
             Console.WriteLine();
             Console.WriteLine($"You guessed: {retries} times!");
             Console.WriteLine();
@@ -71,7 +72,7 @@ namespace guessingGame2
                 goto askAgain;
             }
 
-            if (yourInt == cpRandomInt  + 1 || yourInt == cpRandomInt - 1)
+            if (yourInt == cpRandomInt + 1 || yourInt == cpRandomInt - 1)
             {
                 retries++;
                 promptUser("Your guess is wery close to the right answear");
@@ -107,6 +108,32 @@ namespace guessingGame2
                     Console.WriteLine("Bye!");
                 }
             }
+        }
+
+        private static void welcomeScreen()
+        {
+
+        void marginsText(string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(text);
+        }
+
+        marginsText(@" .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------. ",9,10);
+        marginsText(@"| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |",9,11);
+        marginsText(@"| |  _______     | || |      __      | || | ____  _____  | || |  ________    | || |     ____     | || | ____    ____ | |",9,12);
+        marginsText(@"| | |_   __ \    | || |     /  \     | || ||_   \|_   _| | || | |_   ___ `.  | || |   .'    `.   | || ||_   \  /   _|| |",9,13);
+        marginsText(@"| |   | |__) |   | || |    / /\ \    | || |  |   \ | |   | || |   | |   `. \ | || |  /  .--.  \  | || |  |   \/   |  | |",9,14);
+        marginsText(@"| |   |  __ /    | || |   / ____ \   | || |  | |\ \| |   | || |   | |    | | | || |  | |    | |  | || |  | |\  /| |  | |",9,15);
+        marginsText(@"| |  _| |  \ \_  | || | _/ /    \ \_ | || | _| |_\   |_  | || |  _| |___.' / | || |  \  `--'  /  | || | _| |_\/_| |_ | |",9,16);
+        marginsText(@"| | |____| |___| | || ||____|  |____|| || ||_____|\____| | || | |________.'  | || |   `.____.'   | || ||_____||_____|| |",9,17);
+        marginsText(@"| |              | || |              | || |              | || |              | || |              | || |              | |",9,18);
+        marginsText(@"| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |",9,19);
+        marginsText(@" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ",9,20);
+        
+        marginsText("Take a Guess!!!",60,26);
+        marginsText("Press any key to start a new game",50,28);
+        Console.ReadKey();
         }
     }
 }
